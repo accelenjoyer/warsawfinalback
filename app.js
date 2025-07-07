4eeb82d54dc0c1910fe58711c269f10931e8aef8
accelenjoyer <135505929+accelenjoyer@users.noreply.github.com>
2025-07-08 00:02:14 +0300

    ы

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+    origin: true // Разрешить все домены
+}));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use('/auth', authRoutes);
